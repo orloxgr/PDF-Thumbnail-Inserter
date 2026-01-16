ca9461924a9f79798a65ac3bb64053d37f5d893a
orloxgr <32058744+orloxgr@users.noreply.github.com>
2026-01-16 13:22:51 +0200

    Update pdf-thumbnail.js

--- js/pdf-thumbnail.js
+++ js/pdf-thumbnail.js
@@ -11,17 +11,31 @@ jQuery(document).ready(function ($) {
 
             $.post(pdfThumbnail.ajax_url, {
                 action: 'fetch_pdf_data',
-                attachment_id: attachment.id
+                attachment_id: attachment.id,
+                nonce: pdfThumbnail.nonce
             }, function (response) {
-                if (response.success) {
-                    let shortcode = `[pdf_thumbnail thumbnail="${response.data.thumbnail_url}" title="${response.data.file_title}" url="${response.data.file_url}"]`;
+                if (response && response.success) {
+                    const escAttr = (v) => String(v || '').replace(/"/g, '&quot;');
+
+                    let shortcode = `[pdf_thumbnail thumbnail="${escAttr(response.data.thumbnail_url)}" title="${escAttr(response.data.file_title)}" url="${escAttr(response.data.file_url)}"]`;
                     wp.media.editor.insert(shortcode);
                 } else {
-                    alert(response.data.message);
+                    alert(response && response.data && response.data.message ? response.data.message : 'Error.');
                 }
             });
         });
 
         frame.open();
+    });
+
+    // Hover colors without inline JS
+    $(document).on('mouseenter', '.pdf-download-button', function () {
+        const hover = $(this).attr('data-hover');
+        if (hover) $(this).css('background-color', hover);
+    });
+
+    $(document).on('mouseleave', '.pdf-download-button', function () {
+        const normal = $(this).attr('data-normal');
+        if (normal) $(this).css('background-color', normal);
     });
 });
