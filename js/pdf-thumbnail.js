932ff162732ed67d99feb27c74c586f1756c1263
orloxgr <32058744+orloxgr@users.noreply.github.com>
2026-04-07 14:50:15 +0300

    Update pdf-thumbnail.js

--- js/pdf-thumbnail.js
+++ js/pdf-thumbnail.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
     $('#insert-pdf-thumbnail').on('click', function () {
         let frame = wp.media({
-            title: 'Select PDF File',
+            title: wp.i18n ? wp.i18n.__('Select PDF File', 'pdf-thumbnail-inserter') : 'Select PDF File',
             library: { type: 'application/pdf' },
             multiple: false
         });
@@ -20,22 +20,11 @@ jQuery(document).ready(function ($) {
                     let shortcode = `[pdf_thumbnail thumbnail="${escAttr(response.data.thumbnail_url)}" title="${escAttr(response.data.file_title)}" url="${escAttr(response.data.file_url)}"]`;
                     wp.media.editor.insert(shortcode);
                 } else {
-                    alert(response && response.data && response.data.message ? response.data.message : 'Error.');
+                    alert(response && response.data && response.data.message ? response.data.message : 'An error occurred.');
                 }
             });
         });
 
         frame.open();
-    });
-
-    // Hover colors without inline JS
-    $(document).on('mouseenter', '.pdf-download-button', function () {
-        const hover = $(this).attr('data-hover');
-        if (hover) $(this).css('background-color', hover);
-    });
-
-    $(document).on('mouseleave', '.pdf-download-button', function () {
-        const normal = $(this).attr('data-normal');
-        if (normal) $(this).css('background-color', normal);
     });
 });
